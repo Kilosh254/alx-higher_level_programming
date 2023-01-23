7c1da5e3feeb0b1f2f2083eb0520b31d5efb146d
Kilosh254 <Me.kilonzo@gmail.com>
2023-01-23 12:24:28 +0000

    Task 0_new

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-idx = 0
+j = 0
 try:
-for i in my_list:
-if idx < x:
-print('{}'.format(my_list[idx]), end='')
-idx += 1
-print()
-except TypeError:
-pass
-finally:
-return idx
+for i in range(0, x):
+print("{}".format(my_list[i]), end="")
+j += 1
+print("")
+return x
+except IndexError:
+print("")
+return j
